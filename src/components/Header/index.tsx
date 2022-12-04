1d362ccaf455df0b0fbbef1adce60fa97f04f2aa
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-04 18:31:30 -0300

    feat: create todo is now functional

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useContext, useRef } from 'react';
 
 import lightMode from '../../assets/icon-sun.svg';
 import darkMode from '../../assets/icon-moon.svg';
 
 import * as S from './styles';
 import Activity from '../Activity';
+import { createActivity } from '../../services/createActivity';
+import { ActivitiesContext } from '../../context/ActivityContext';
+import { getActivities } from '../../services/getActivities';
 
 export function Header() {
+  const message = useRef<HTMLInputElement | null>(null);
+  const {setActivities} = useContext(ActivitiesContext);
+
+  const handleSubmit = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && message.current) {
+      createActivity(message.current.value).then(() => {
+        getActivities().then(response => setActivities(response));
+      });
+    }
+  };
+
   return (
     <S.Container>
       <S.ContentSection>
@@ -16,7 +30,10 @@ export function Header() {
         </S.Logo>
 
         <Activity borderRadius={8}>
-          <S.Input placeholder='Create a new todo...'/>
+          <S.Input
+            placeholder='Create a new todo...'
+            ref={message}
+            onKeyPress={e => handleSubmit(e)}/>
         </Activity>
       </S.ContentSection>
     </S.Container>
