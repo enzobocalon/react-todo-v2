1de3306e0780bb43fe1f1475f5004b446807707f
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-04 19:48:05 -0300

    feat: header can reset active state

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,10 @@
 import React, { useContext, useRef } from 'react';
+import * as S from './styles';
 
 import lightMode from '../../assets/icon-sun.svg';
 import darkMode from '../../assets/icon-moon.svg';
 
-import * as S from './styles';
+
 import Activity from '../Activity';
 import { createActivity } from '../../services/createActivity';
 import { ActivitiesContext } from '../../context/ActivityContext';
@@ -11,11 +12,13 @@ import { getActivities } from '../../services/getActivities';
 
 export function Header() {
   const message = useRef<HTMLInputElement | null>(null);
-  const {setActivities} = useContext(ActivitiesContext);
+  const {setActivities, setActive} = useContext(ActivitiesContext);
 
   const handleSubmit = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter' && message.current) {
       createActivity(message.current.value).then(() => {
+        message.current!.value = '';
+        setActive(0);
         getActivities().then(response => setActivities(response));
       });
     }
