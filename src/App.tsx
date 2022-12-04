4956fd7a31b4c4c614be465465093b50cf4a0bed
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-04 17:07:58 -0300

    feat: activities context

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { ActivityList } from './components/ActivityList';
 import { Header } from './components/Header';
+import ActivitiesProvider from './context/ActivityContext';
 import ModesProvider from './context/ModesContext';
 import { GlobalStyle } from './Global';
 
 function App() {
-
-
   return (
     <>
       <ModesProvider>
-        <Header />
-        <ActivityList />
-        <GlobalStyle />
+        <ActivitiesProvider>
+          <Header />
+          <ActivityList />
+          <GlobalStyle />
+        </ActivitiesProvider>
       </ModesProvider>
     </>
   );
