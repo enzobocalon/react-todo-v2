2a140bca25d841f0c4a5e4abcfa9f689c9d0737c
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-04 19:47:42 -0300

    feat: active state

--- src/context/ActivityContext.tsx
+++ src/context/ActivityContext.tsx
@@ -3,7 +3,9 @@ import { Activity } from '../types/Activity';
 
 interface IModes {
   activities: Activity[] | null,
-  setActivities: React.Dispatch<React.SetStateAction<Activity[] | null>>
+  setActivities: React.Dispatch<React.SetStateAction<Activity[] | null>>,
+  active: number,
+  setActive: React.Dispatch<React.SetStateAction<number>>
 }
 
 type ProviderProps = {
@@ -14,12 +16,15 @@ export const ActivitiesContext = createContext({} as IModes);
 
 const ActivitiesProvider = ({children}: ProviderProps) => {
   const [activities, setActivities] = useState<Activity[] | null>(null);
+  const [active, setActive] = useState<number>(0);
 
   return (
     <ActivitiesContext.Provider
       value={{
         activities,
-        setActivities
+        setActivities,
+        active,
+        setActive
       }}
     >
       {children}
