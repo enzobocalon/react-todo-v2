153e96e219ce1f51b1127a6783c84f937b9deb40
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-04 20:42:04 -0300

    refactor: fixes message crashing

--- src/services/createActivity.ts
+++ src/services/createActivity.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 export const createActivity = async (message: string) => {
-  await axios.post('http://localhost:3001', {
-    message: message,
-    status: 0,
-    isDeletable: true
-  });
+  if (message) {
+    await axios.post('http://localhost:3001', {
+      message: message,
+      status: 0,
+      isDeletable: true
+    });
+  }
 };
