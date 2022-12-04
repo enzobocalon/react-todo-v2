4076756f5b7ef9b434abb9b78ab0914dfd04f213
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-04 16:04:51 -0300

    feat: get data from api

---
+++ src/services/getActivities.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const getActivities = async () => {
+  const response = await axios.get('http://localhost:3001/');
+  if (response) {
+    return response.data;
+  } else {
+    console.log('❌ Failing fetching data.');
+  }
+};
