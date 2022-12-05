7798d450d4f9f1c5a44233e886b3c0c083054f6b
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-05 12:27:24 -0300

    feat: mocks

---
+++ src/mocks/Activity.ts
@@ -0,0 +1,44 @@
+export const ActivityMock = [
+  {
+    '_id': '638d6b8100a53b74339f5807',
+    'message': 'Complete online JavaScript Course',
+    'status': 1,
+    'isDeletable': false,
+    '__v': 0
+  },
+  {
+    '_id': '638d6b8900a53b74339f5809',
+    'message': 'Jog around the park 3x',
+    'status': 0,
+    'isDeletable': false,
+    '__v': 0
+  },
+  {
+    '_id': '638d6b8f00a53b74339f580b',
+    'message': '10 minutes meditation',
+    'status': 0,
+    'isDeletable': false,
+    '__v': 0
+  },
+  {
+    '_id': '638d6b9500a53b74339f580d',
+    'message': 'Read for 1 hour',
+    'status': 0,
+    'isDeletable': false,
+    '__v': 0
+  },
+  {
+    '_id': '638d6b9d00a53b74339f580f',
+    'message': 'Pick up groceries',
+    'status': 0,
+    'isDeletable': false,
+    '__v': 0
+  },
+  {
+    '_id': '638d6ba600a53b74339f5811',
+    'message': 'Complete Todo App on Frontend Mentor',
+    'status': 0,
+    'isDeletable': false,
+    '__v': 0
+  }
+];
